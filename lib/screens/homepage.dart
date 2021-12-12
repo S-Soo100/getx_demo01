f1ac7d24ace4be63f567a95de4e076011b87db8e
S-Soo100 <bss.rol20@gmail.com>
2021-12-13 00:41:55 +0900

    a

--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -8,7 +7,14 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: Column(),
+      body: Column(
+        children: [
+          MaterialButton(
+            onPressed: (){},
+          ),
+        ],
+
+      ),
     );
   }
 }
